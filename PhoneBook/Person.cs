94d6602d5eb966107d2963fe628f407ae8dcb9fa
Starinin Andrey <starininandrey@gmail.com>
2024-10-18 19:06:23 +0300

    Add Person.cs

---
+++ PhoneBook/Person.cs
@@ -0,0 +1,56 @@
+﻿namespace PhoneBook;
+
+public class Person
+{
+    public Guid Id { get; set; }
+
+    private string _firstName;
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                _firstName = value;
+            }
+        }
+    }
+    
+    private string _lastName;
+    public string LastName
+    {
+        get => _lastName;
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                _lastName = value;
+            }
+        }
+    }
+    
+    public string FullName => $"{FirstName} {LastName}";
+    
+    private DateTime _birthDate;
+    public DateTime BirthDate
+    {
+        get => _birthDate;
+        set
+        {
+            if (value <= DateTime.Now)
+            {
+                _birthDate = value;
+            }
+        } 
+    }
+    private int GetAge()
+    {
+        var today = DateTime.Today;
+        var age = today.Year - BirthDate.Year;
+        if (BirthDate.Date > today.AddYears(-age)) age--;
+        
+        return age;
+    }
+    public int Age => GetAge();
+}
