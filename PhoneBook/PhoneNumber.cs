d843270a3e794ea05e3a1f48dd16a971985c01f2
Starinin Andrey <starininandrey@gmail.com>
2024-10-18 19:55:41 +0300

    Add PhoneNumber.cs

---
+++ PhoneBook/PhoneNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace PhoneBook;
+
+public enum TypeOfPhoneNumber
+{
+    Unknown,
+    Home,
+    Mobile,
+    Work
+}
+
+public class PhoneNumber
+{
+    public required TypeOfPhoneNumber Type { get; init; }
+    
+    private readonly string _number;
+    public required string Number { 
+        get => _number;
+        init
+        {
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Телефонный номер не может быть пустым");
+            }
+            
+            _number = value;
+        } 
+    }
+}
